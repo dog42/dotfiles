3039a84911eb45ff35cb0d6bb2453d81eb888d19
dock <philipp.dockhorn@hs-merseburg.de>
2026-04-23 16:00:53 +0200

    nvim move to window by <leader>+w, direction (hjkl)

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -9,3 +9,18 @@ vim.keymap.set("n", "<S-Left>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
 vim.keymap.set("n", "<S-Right>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
 -- vim.keymap.set("n", "<cr>", "za", { desc = "toggle fold" })
 vim.keymap.set("n", "<leader>h", Snacks.dashboard.open, { desc = "Dashboard/Open mini starter" })
+
+-- Fensterwechsel mit Space + w + Pfeiltasten
+vim.keymap.set("n", "<Leader>w<Left>", "<C-w>h", { desc = "Fenster links" })
+vim.keymap.set("n", "<Leader>w<Down>", "<C-w>j", { desc = "Fenster unten" })
+vim.keymap.set("n", "<Leader>w<Up>", "<C-w>k", { desc = "Fenster oben" })
+vim.keymap.set("n", "<Leader>w<Right>", "<C-w>l", { desc = "Fenster rechts" })
+
+-- Zusätzlich die Buchstaben-Variante (h,j,k,l) - falls du sie mal brauchst
+vim.keymap.set("n", "<Leader>wh", "<C-w>h", { desc = "Fenster links" })
+vim.keymap.set("n", "<Leader>wj", "<C-w>j", { desc = "Fenster unten" })
+vim.keymap.set("n", "<Leader>wk", "<C-w>k", { desc = "Fenster oben" })
+vim.keymap.set("n", "<Leader>wl", "<C-w>l", { desc = "Fenster rechts" })
+
+-- Das generische Wechseln mit Space + w + w
+vim.keymap.set("n", "<Leader>ww", "<C-w>w", { desc = "Nächstes Fenster" })
