aad18ceb43da67568ec02e508233a3b5a50bad6c
dock <philipp.dockhorn@hs-merseburg.de>
2026-04-23 14:18:50 +0200

    install platformio and link path; remove old keybinds for nvim

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -2,9 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 -- vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
-vim.keymap.set("n", "<leader>mm", ":w<CR>:copen<CR><ESC>:AsyncRun make<CR>", { desc = "run make async" })
-vim.keymap.set("n", "<leader>mu", ":w<CR>:copen<CR><ESC>:AsyncRun make upload<CR>", { desc = "run make upload async" })
-vim.keymap.set("n", "<leader>mc", ":copen<CR><ESC>:PIOMonitor<CR>", { desc = "run PIOMonitor" })
+-- vim.keymap.set("n", "<leader>mm", ":w<CR>:copen<CR><ESC>:AsyncRun make<CR>", { desc = "run make async" })
+-- vim.keymap.set("n", "<leader>mu", ":w<CR>:copen<CR><ESC>:AsyncRun make upload<CR>", { desc = "run make upload async" })
+-- vim.keymap.set("n", "<leader>mc", ":copen<CR><ESC>:PIOMonitor<CR>", { desc = "run PIOMonitor" })
 vim.keymap.set("n", "<S-Left>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
 vim.keymap.set("n", "<S-Right>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
 -- vim.keymap.set("n", "<cr>", "za", { desc = "toggle fold" })
