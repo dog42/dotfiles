9a4f222b5f3ffa124a801f365de4ac7713cd3742
dock <philipp.dockhorn@hs-merseburg.de>
2026-08-06 11:55:17 +0200

    helper (hyprland) parse monitor.conf in monitor.lua

--- dot_config/hypr/workspaces.lua
+++ dot_config/hypr/workspaces.lua
@@ -1,1 +1,4 @@
 -- workspaces configurations (currently empty)
+hl.workspace_rule({ workspace = "1", monitor = "DP-8"})
+hl.workspace_rule({ workspace = "2", monitor = "DP-7"})
+hl.workspace_rule({ workspace = "3", monitor = "eDP-1"})
